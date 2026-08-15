29d2d215c1f6ece31590fd87ca605eef30385d6e
Fabrizio De Vincenzi <hello@fabriziodevincenzi.com>
2026-08-15 16:34:40 +0200

    Add Turnstile protection to waitlist

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly PUBLIC_WAITLIST_COUNT?: string;
   readonly PUBLIC_SUPABASE_URL?: string;
   readonly PUBLIC_SUPABASE_PUBLISHABLE_KEY?: string;
+  readonly PUBLIC_TURNSTILE_SITE_KEY?: string;
 }
 
 interface ImportMeta {
